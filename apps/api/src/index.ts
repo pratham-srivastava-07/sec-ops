6c0471d0036668f49cd354a113e683ecf0ebea6d
Krish <prathamrajsri2003@gmail.com>
2025-06-25 02:32:50 +0530

    init

---
+++ apps/api/src/index.ts
@@ -0,0 +1,11 @@
+import { prismaClient } from "@ops/shared";
+import express from "express"
+import { PORT } from "./constants";
+
+const app = express()
+
+app.get("/", async (req, res) => {
+    res.send("hello")
+})
+
+app.listen(PORT, () =>  console.log("api server started"))
